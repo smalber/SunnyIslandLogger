0d4b34d10b7495194ef682022dfde3374430d29f
smalber <reno.smalberger@gmail.com>
2019-03-19 15:09:13 +0200

    Update smysqloggerd.c

--- yasdi/logger/smysqloggerd.c
+++ yasdi/logger/smysqloggerd.c
@@ -87,6 +87,20 @@ char dbColumns[][MAX_COLUMN_NAME_LEN] =
 char dbTypes[][MAX_COLUMN_TYPE_LEN] = 
 {"SMALLINT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "FLOAT UNSIGNED", "FLOAT UNSIGNED", "DOUBLE UNSIGNED", "DOUBLE UNSIGNED", "DOUBLE UNSIGNED", "MEDIUMINT UNSIGNED", "BIGINT UNSIGNED", "VARCHAR(10)", "VARCHAR(10)", "VARCHAR(10)"};
 
+
+// Channel names used by Sunny Island
+char SIchannelNames[][MAX_CHANNEL_NAME_LEN] = 
+{"Upv-Ist", "Upv-Soll", "Iac-Ist", "Uac", "Fac", "Pac", "Zac", "Riso", "Ipv", "E-Total", "h-Total", "h-On", "Netz-Ein", "Seriennummer", "Status", "Balancer", "Fehler"};
+
+// Names of the database columns to store the values in
+char SIdbColumns[][MAX_COLUMN_NAME_LEN] = 
+{"upv_ist_volt", "upv_soll_volt", "iac_ist_amp", "uac_volt", "fac_hz", "pac_watt", "zac_ohm", "riso_kohm", "ipv_amp", "e_total_kwh", "h_total_hour", "h_on_hour", "netz_ein", "seriennummer", "status", "balancer", "fehler"};
+
+// Types of the database columns to store the values in
+char SIdbTypes[][MAX_COLUMN_TYPE_LEN] = 
+{"SMALLINT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "SMALLINT UNSIGNED", "FLOAT UNSIGNED", "FLOAT UNSIGNED", "FLOAT UNSIGNED", "DOUBLE UNSIGNED", "DOUBLE UNSIGNED", "DOUBLE UNSIGNED", "MEDIUMINT UNSIGNED", "BIGINT UNSIGNED", "VARCHAR(10)", "VARCHAR(10)", "VARCHAR(10)"};
+
+
 /* maximum age of the channel values, in seconds...*/
 DWORD maxChannelValueAge; 
 
@@ -183,6 +197,35 @@ int checkArrayConsistency() {
 	}
 	
 	return channelCount;
+}
+
+/**************************************************************************
+SIcheckArrayConsistency
+**************************************************************************/
+int SIcheckArrayConsistency() {
+	int SIchannelCount = sizeof(SIchannelNames)/MAX_CHANNEL_NAME_LEN;
+	int SIcolumnCount = sizeof(SIdbColumns)/MAX_COLUMN_NAME_LEN;
+	int SItypeCount = sizeof(SIdbTypes)/MAX_COLUMN_TYPE_LEN;
+	int i;
+
+	printLog(LEVEL_DETAIL, "List of requested channels:\n");
+    lightLog(LEVEL_DETAIL, "        SIchannel_name -> db_column (db_type)\n");
+    lightLog(LEVEL_DETAIL, "------------------------------------------------------------------------------------\n");
+	for (i = 0; i < max(SIchannelCount, max(SIcolumnCount, SItypeCount)); i++) {
+		lightLog(LEVEL_DETAIL, "%20s -> %s (%s)\n", (i < SIchannelCount ? SIchannelNames[i]:"?"), (i < SIcolumnCount ? SIdbColumns[i]:"?"), (i < SItypeCount ? SIdbTypes[i]:"?"));
+	}
+
+	// Check size consistency
+	if (SIchannelCount != SIcolumnCount) { 
+		printLog(LEVEL_FATAL, "Error : inconsistent number of SIchannel_names (%d) and SIdb_columns (%d)!\n", SIchannelCount, SIcolumnCount);
+		exit(-20);
+	}
+	if (SIcolumnCount != SItypeCount) { 
+		printLog(LEVEL_FATAL, "Error : inconsistent number of SIdb_columns (%d) and SIdb_types (%d)!\n", SIcolumnCount, SItypeCount);
+		exit(-21);
+	}
+	
+	return SIchannelCount;
 }
 
 
@@ -315,6 +358,134 @@ BOOL logValues(int deviceCount, DWORD deviceHandles[], int channelCount) {
 }
 
 /**************************************************************************
+SIlogValues
+**************************************************************************/
+BOOL SIlogValues(int SIdeviceCount, DWORD SIdeviceHandles[], int SIchannelCount) {
+	int res, i;
+
+	char SIchannelValues[SIchannelCount][MAX_CHANNEL_VALUE_LEN];
+	char SIchanName[MAX_CHANNEL_NAME_LEN];
+	DWORD SIchannelHandles[SIdeviceCount][SIchannelCount]; 
+	double value;
+	char textValue[MAX_CHANNEL_VALUE_LEN];
+	time_t loopStart, dayStart, now;
+	double elapsedSecs;
+	int SIdeviceNr, SIchannelNr, delay;
+
+	/* init channel handles */
+	for (SIdeviceNr = 0; SIdeviceNr < SIdeviceCount; SIdeviceNr++) {
+		for (SIchannelNr = 0; SIchannelNr < SIchannelCount; SIchannelNr++) {
+			SIchannelHandles[SIdeviceNr][SIchannelNr] = FindChannelName(SIdeviceHandles[SIdeviceNr], SIchannelNames[SIchannelNr]);
+			if (SIchannelHandles[SIdeviceNr][SIchannelNr] <= 0) {
+				// Throw in some help for debugging purpose...
+				DWORD SIchanHandle[300]; 
+				char SIdeviceName[50];
+				char cUnit[17];
+				int SIrealChannelNr;
+
+				printLog(LEVEL_FATAL, "Error : cannot determine handle of channel name '%s' for device %d\n", SIchannelNames[SIchannelNr], SIdeviceHandles[SIdeviceNr]);
+				int SIrealChannelCount = GetChannelHandlesEx(SIdeviceHandles[SIdeviceNr], SIchanHandle, 300, SPOTCHANNELS);
+				GetDeviceName(SIdeviceHandles[SIdeviceNr], SIdeviceName, sizeof(SIdeviceName)-1);
+				lightLog(LEVEL_ERROR, "Here are the %d available SI channels for device '%s':\n", SIrealChannelCount, SIdeviceName);
+				lightLog(LEVEL_ERROR, "---------------------------------------\n");
+				lightLog(LEVEL_ERROR, "|   SI Channel name   |   SI Channel Unit   |\n");
+				lightLog(LEVEL_ERROR, "---------------------------------------\n");
+				for(SIrealChannelNr = 0; SIrealChannelNr < SIrealChannelCount; SIrealChannelNr++)
+				{
+					GetChannelName(SIchanHandle[SIrealChannelNr], SIchanName, sizeof(SIchanName)-1);
+					cUnit[0]=0;
+					GetChannelUnit(SIchanHandle[SIrealChannelNr], cUnit, sizeof(cUnit)-1);
+					lightLog(LEVEL_ERROR, "| %16s | %-16s |\n", SIchanName, cUnit);
+				}
+				lightLog(LEVEL_ERROR, "---------------------------------------\n");
+
+				exit(-50);
+			}
+			GetChannelName(SIchannelHandles[SIdeviceNr][SIchannelNr], SIchanName, sizeof(SIchanName)-1);
+			printLog(LEVEL_DETAIL, "SI Channel '%s' has handle %d on device %d\n", SIchannelNames[SIchannelNr], SIchannelHandles[SIdeviceNr][SIchannelNr], SIdeviceHandles[SIdeviceNr]);
+		}
+	}
+
+	BOOL readOK = true;
+	
+	// Determine first schedule
+	now = time(NULL);
+	struct tm *cal = localtime(&now);
+	cal->tm_hour = 0;
+	cal->tm_min = 0;
+	cal->tm_sec = 0;
+	dayStart = mktime(cal);
+	elapsedSecs = difftime(now, dayStart);
+
+	delay = requestedInterval - ((int)(elapsedSecs) % requestedInterval); 
+	printLog(LEVEL_INFO, "Waiting for %d seconds before first trace...\n", delay);
+	// Sleep by 1-second interval to allow exit if requested
+	for(i=0; (i<delay) && loop; i++) {
+		sleep(1);
+	}
+
+	printLog(LEVEL_INFO, "Starting loop. Each + below represents a trace successfully inserted in DB:\n", delay);
+	while (loop) {
+		loopStart = time(NULL);
+		
+		// Iterate on devices
+		for (SIdeviceNr = 0; SIdeviceNr < SIdeviceCount; SIdeviceNr++) {
+
+            // Iterate on channels for this device
+			for (SIchannelNr=0; SIchannelNr<SIchannelCount; SIchannelNr++) {
+				res = GetChannelValue(SIchannelHandles[SIdeviceNr][SIchannelNr], SIdeviceHandles[SIdeviceNr], &value, textValue, sizeof(textValue)-1, maxChannelValueAge);
+				if (res == 0) {
+					if (strlen(textValue) == 0) {
+						sprintf(textValue, "%.6f", value);
+					}
+				}
+				else {                    
+                    // means connection with at least one device was lost. 
+                    if (res != -3) {
+                        printLog(LEVEL_WARNING, "Error reading channel value....error code=%d\n", res);
+                    }
+                    readOK = false;
+                    loop = false;
+					break;
+				}
+				strcpy(SIchannelValues[SIchannelNr], textValue);
+			}
+			
+			if (readOK) {
+				// Insert row
+				SIinsertValues(SIchannelCount, SIdbColumns, SIchannelValues);
+				lightLog(LEVEL_INFO, "+");
+			}
+		}
+
+		if (loop) {
+            // determine processing duration to schedule next loop turn
+            now = time(NULL);
+            elapsedSecs = difftime(now, loopStart);
+
+            if (elapsedSecs < requestedInterval) {
+                // Compute theoretic start of next loop
+                struct tm *cal = localtime(&loopStart);
+                cal->tm_sec += requestedInterval;
+                loopStart = mktime(cal);
+                delay = requestedInterval - elapsedSecs;
+
+                // Must sleep now.
+				// Sleep by 1-second interval to allow exit if requested
+				for(i=0; (i<delay) && loop; i++) {
+					sleep(1);
+				}
+            }
+            else {
+                printLog(LEVEL_WARNING, "Warning : Processing took %f seconds. Can't keep up with the %d seconds requested interval. Please choose a higher value.\n", elapsedSecs, requestedInterval);
+                loopStart = time(NULL);
+            }
+        }
+	}
+    return readOK;
+}
+
+/**************************************************************************
 * startDeviceLogging
 * Start synchronous device detection (blocks until device detection is done)
 * then launches logging
@@ -323,7 +494,7 @@ void startDeviceLogging(int deviceCount, DWORD deviceHandles[], int channelCount
 	int iErrorCode, deviceNr;
 	char nameBuffer[50];
 
-	printLog(LEVEL_INFO, "Starting detection of %d device(s). Please wait...\n", deviceCount);
+	printLog(LEVEL_INFO, "Starting detection of %d SB device(s). Please wait...\n", deviceCount);
 
 	/* start searching for devices... */
 	BOOL deviceOK = false;
@@ -337,7 +508,7 @@ void startDeviceLogging(int deviceCount, DWORD deviceHandles[], int channelCount
 				break;
 			 
 			case YE_DEV_DETECT_IN_PROGRESS:
-				printLog(LEVEL_FATAL, "\nError: there is already an running device detection.\n");
+				printLog(LEVEL_FATAL, "\nError: there is already an running SB device detection.\n");
 				exit(-40);
 		  
 			case YE_OK:
@@ -355,13 +526,13 @@ void startDeviceLogging(int deviceCount, DWORD deviceHandles[], int channelCount
             DWORD devCount = GetDeviceHandles(deviceHandles, deviceCount);
                 
             if (devCount != deviceCount) {
-                printLog(LEVEL_FATAL, "Error : GetDeviceHandles returned %d device(s) while we expect %d\n", devCount, deviceCount);
+                printLog(LEVEL_FATAL, "Error : GetDeviceHandles returned %d SB device(s) while we expect %d\n", devCount, deviceCount);
                 exit(-42);
             }
 
             // Debug output
             lightLog(LEVEL_DEBUG, "-------------------------------------------\n");
-            lightLog(LEVEL_DEBUG, "Device handle | Device Name  \n");
+            lightLog(LEVEL_DEBUG, "SB Device handle | SB Device Name  \n");
             lightLog(LEVEL_DEBUG, "-------------------------------------------\n");
             for (deviceNr = 0; deviceNr < deviceCount; deviceNr++) {
                 GetDeviceName(deviceHandles[deviceNr], nameBuffer, sizeof(nameBuffer)-1);
@@ -373,6 +544,67 @@ void startDeviceLogging(int deviceCount, DWORD deviceHandles[], int channelCount
         }
     }
 }
+
+/**************************************************************************
+* SIstartDeviceLogging
+* Start synchronous device detection (blocks until device detection is done)
+* then launches logging
+**************************************************************************/
+void SIstartDeviceLogging(int SIdeviceCount, DWORD SIdeviceHandles[], int SIchannelCount) {
+	int SIiErrorCode, SIdeviceNr;
+	char SInameBuffer[50];
+
+	printLog(LEVEL_INFO, "Starting detection of %d SI device(s). Please wait...\n", SIdeviceCount);
+
+	/* start searching for devices... */
+	BOOL deviceOK = false;
+	while (!deviceOK) {
+		SIiErrorCode = DoStartDeviceDetection(SIdeviceCount, TRUE /*blocking*/ );
+		switch(SIiErrorCode) {
+			case YE_NOT_ALL_DEVS_FOUND:
+				lightLog(LEVEL_INFO, "-");
+				sleep(requestedInterval);
+				deviceOK = false;	
+				break;
+			 
+			case YE_DEV_DETECT_IN_PROGRESS:
+				printLog(LEVEL_FATAL, "\nError: there is already an running device SI detection.\n");
+				exit(-40);
+		  
+			case YE_OK:
+				deviceOK = true;	
+				break;
+			 
+			default:
+				printLog(LEVEL_FATAL, "\nUnknown error : %d\n", SIiErrorCode);
+				exit(-41);
+		}
+        
+        if (deviceOK) {
+			lightLog(LEVEL_INFO, "\n");
+            /* get all device handles...*/
+            DWORD SIdevCount = GetDeviceHandles(SIdeviceHandles, SIdeviceCount);
+                
+            if (SIdevCount != SIdeviceCount) {
+                printLog(LEVEL_FATAL, "Error : GetDeviceHandles returned %d SI device(s) while we expect %d\n", SIdevCount, SIdeviceCount);
+                exit(-42);
+            }
+
+            // Debug output
+            lightLog(LEVEL_DEBUG, "-------------------------------------------\n");
+            lightLog(LEVEL_DEBUG, "SI Device handle | SI Device Name  \n");
+            lightLog(LEVEL_DEBUG, "-------------------------------------------\n");
+            for (SIdeviceNr = 0; SIdeviceNr < SIdeviceCount; SIdeviceNr++) {
+                GetDeviceName(SIdeviceHandles[SIdeviceNr], nameBuffer, sizeof(nameBuffer)-1);
+                lightLog(LEVEL_DEBUG, "   %3lu        | '%s'\n", (unsigned long)SIdeviceHandles[SIdeviceNr], nameBuffer);
+            }
+            lightLog(LEVEL_DEBUG, "-------------------------------------------\n\n");
+            
+            deviceOK = SIlogValues(SIdeviceCount, SIdeviceHandles, SIchannelCount);
+        }
+    }
+}
+
 
 /**************************************************************************
 usage
@@ -499,22 +731,35 @@ int main(int argc, char *argv[])
 	
 	// Check arrays consistency 
 	int channelCount = checkArrayConsistency();
+	int SIchannelCount = SIcheckArrayConsistency();
 	
 	// Init DB
 	dbInit(iniFile, channelCount, dbColumns, dbTypes);
+	SIdbInit(iniFile, SIchannelCount, SIdbColumns, SIdbTypes);
 
-	// Autodetect devices when started
+	// Autodetect SB devices when started
     int deviceCount = GetPrivateProfileInt_("Plant", "deviceCount", -1, iniFile);
 	if (deviceCount == -1) {
 		printLog(LEVEL_FATAL, "Error: 'deviceCount=xx' must be specified under section '[Plant]' of %s !\n", iniFile);
 		exit(-4);
 	}
+	
+	// Autodetect SI devices when started
+    int SIdeviceCount = GetPrivateProfileInt_("Plant", "SIdeviceCount", -1, iniFile);
+	if (SIdeviceCount == -1) {
+		printLog(LEVEL_FATAL, "Error: 'SIdeviceCount=xx' must be specified under section '[Plant]' of %s !\n", iniFile);
+		exit(-4);
+	}
 
    	DWORD deviceHandles[deviceCount];
+	DWORD SIdeviceHandles[SIdeviceCount];
 
 
-	/* Detect devices and start logging */
+	/* Detect SB devices and start logging */
 	startDeviceLogging(deviceCount, deviceHandles, channelCount);
+	
+	/* Detect SI devices and start logging */
+	SIstartDeviceLogging(SIdeviceCount, SIdeviceHandles, SIchannelCount);
 
 
 	
